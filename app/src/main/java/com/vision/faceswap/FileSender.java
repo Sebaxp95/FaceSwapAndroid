ef86150ed7235f1d32134caeb4228e98d24ddb3e
hector <td52qop/c>
2019-01-30 16:55:10 -0500

    posting for image fixed

--- app/src/main/java/com/vision/faceswap/FileSender.java
+++ app/src/main/java/com/vision/faceswap/FileSender.java
@@ -1,14 +1,13 @@
 package com.vision.faceswap;
 
-import com.google.common.io.Files;
-import com.google.common.io.Resources;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.core.io.FileSystemResource;
 
 import java.io.File;
 import java.io.IOException;
@@ -16,25 +15,23 @@ import java.io.IOException;
 public class FileSender {
     private static final String URL = "https://face-swap-server.herokuapp.com/swap";
 
-    public File send(File source, File destination) throws IOException {
-        MultiValueMap<String, Object> body = new LinkedMultiValueMap<String, Object>() {{
+    public String send(final File source, final File destination) throws IOException {
+
+        final MultiValueMap<String, Object> body = new LinkedMultiValueMap<String, Object>() {{
             add("src", new FileSystemResource(source));
             add("dst", new FileSystemResource(destination));
         }};
-        HttpHeaders headers = new HttpHeaders() {{
+        final HttpHeaders headers = new HttpHeaders() {{
             setContentType(MediaType.MULTIPART_FORM_DATA);
         }};
 
-        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+        final HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
 
-        RestTemplate restTemplate = new RestTemplate();
-        byte[] response = restTemplate
-                .postForObject(URL, requestEntity, byte[].class);
-        final File outputFile = new File(Resources.getResource("result.jpg").getFile());
+        final RestTemplate restTemplate = new RestTemplate(true);
 
-        Files.write(response, outputFile);
-
-        return outputFile;
+        return restTemplate
+                .exchange(URL, HttpMethod.POST, requestEntity, String.class)
+                .getBody();
     }
 
 }
--- app/src/test/java/com/vision/faceswap/FileSenderTest.java
+++ app/src/test/java/com/vision/faceswap/FileSenderTest.java
@@ -1,11 +1,12 @@
 package com.vision.faceswap;
 
 import com.google.common.io.Resources;
+
 import org.junit.Test;
 
 import java.io.File;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 public class FileSenderTest {
 
@@ -15,9 +16,9 @@ public class FileSenderTest {
     public void test() throws Exception {
         final File dst = new File(Resources.getResource("jack.jpg").getFile());
         final File src = new File(Resources.getResource("test6.jpg").getFile());
-        final File result = fileSender.send(src, dst);
+        final String result = fileSender.send(src, dst);
 
-        assertTrue(true);
+        assertFalse(result.isEmpty());
     }
 
 }
